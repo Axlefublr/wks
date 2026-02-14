e4c31f368a49b2ba793620cb499195a76200d60a
Axlefublr <axlefublr.ls@gmail.com>
2026-02-14 09:44:15 +0800

    wrap-in-block: use helix expansions for indentation characters and first line's indentation level

--- src/bin/wrap-in-block.rs
+++ src/bin/wrap-in-block.rs
@@ -5,32 +5,15 @@
 use itertools::Itertools;
 use wks::prelude::*;
 
-#[derive(PartialEq, Eq, Hash)]
-enum Indent {
-    Tab,
-    Space(usize),
-}
-
-impl Default for Indent {
-    fn default() -> Self {
-        Self::Space(4)
-    }
-}
-
-impl fmt::Display for Indent {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Tab => write!(f, "\t"),
-            Self::Space(how_many) => write!(f, "{}", " ".repeat(*how_many)),
-        }
-    }
-}
-
 fn main() -> Result<()> {
-    let surrounding_left: String = env::args()
-        .nth(1)
+    let args = env::args();
+    let mut args = args.skip(1);
+    let buffer_indentation = args.next().unwrap();
+    let line_indentation = args.next().unwrap();
+    let surrounding_left: String = args
+        .next()
         .and_then(|arg| arg.parse().ok())
-        .unwrap_or("{".into());
+        .unwrap();
     let surrounding_left = match &surrounding_left[..] {
         "b" => "(",
         "B" => "{",
@@ -39,6 +22,10 @@ fn main() -> Result<()> {
         "p" => "|",
         "`" => "```",
         "\"" => r#"""""#,
+        "'" => "'''",
+        "backtick" => "```",
+        "doublequote" => r#"""""#,
+        "singlequote" => "'''",
         other => other,
     };
     let surrounding_right = match surrounding_left {
@@ -46,86 +33,38 @@ fn main() -> Result<()> {
         "{" => "}",
         "<" => ">",
         "[" => "]",
-        "|" => "|",
         "begin" => "end",
-        "```" => "```",
-        r#"""""# => r#"""""#,
-        _ => unimplemented!("(){{}}<>[]||"),
+        other => other,
     };
-    let no_indent_innards = [r#"""""#, "```"];
-    let stdins: Vec<String> = io::stdin()
+    let no_indent_innards = [r#"""""#, "```", "'''"];
+    let indent_innards = no_indent_innards
+        .contains(&surrounding_left)
+        .not();
+    let mut printed_starter = false;
+    for line in io::stdin()
         .lines()
         .map(Result::unwrap)
-        .collect();
-
-    let indentations = stdins
-        .iter()
-        .filter(|&line| !line.trim().is_empty())
-        .map(|line| {
-            line.chars()
-                .take_while(|&chr| chr == ' ' || chr == '\t')
-                .collect::<String>()
-        });
-
-    let indent_type = indentations
-        .clone()
-        .filter(|indent_of_line| !indent_of_line.is_empty())
-        .map(|line_indent| {
-            let len = line_indent.len();
-            if line_indent
-                .chars()
-                .any(|chr| chr == '\t')
-            {
-                Indent::Tab
-            } else if len % 4 == 0 {
-                Indent::Space(4)
-            } else if len % 2 == 0 {
-                Indent::Space(2)
-            } else if len % 3 == 0 {
-                Indent::Space(3)
-            } else {
-                Indent::default()
+    {
+        let contains_own_indent = line.trim_start() != line;
+        if !printed_starter {
+            printed_starter = true;
+            // if selection contains its own indentation, we should also indent our new wrapping characters
+            // if the selection is whitespace-free, we are trying to wrap something in the middle of the line and so should *not*
+            // indent the surrounders that will appear inside of a line
+            if contains_own_indent {
+                print!("{}", line_indentation);
             }
-        })
-        .counts()
-        .into_iter()
-        .max_by_key(|&(_, likelyhood)| likelyhood)
-        .map(|(result_indent, _)| result_indent)
-        .unwrap_or(Indent::default());
-
-    let indent_chars = indent_type.to_string();
-
-    let minimum_indent_level = indentations
-        .map(|indent_line| {
-            let mut count = 0;
-            let mut indent_line = indent_line.as_str();
-            while let Some(rest) = indent_line.strip_prefix(&indent_chars) {
-                count += 1;
-                indent_line = rest;
-            }
-            count
-        })
-        .min()
-        .unwrap_or_default();
-
-    let mut output = String::new();
-    output.push_str(&indent_chars.repeat(minimum_indent_level));
-    output.push_str(surrounding_left);
-    output.push('\n');
-    let source = stdins
-        .into_iter()
-        .fold(String::new(), |mut collector: String, line| {
-            if !no_indent_innards.contains(&surrounding_left) {
-                collector.push_str(&indent_chars);
-            }
-            collector.push_str(&line);
-            collector.push('\n');
-            collector
-        });
-    output.push_str(&source);
-    output.push_str(&indent_chars.repeat(minimum_indent_level));
-    output.push_str(surrounding_right);
-    output.push('\n');
-    print!("{output}");
+            println!("{}", surrounding_left);
+        }
+        if !contains_own_indent {
+            print!("{}", line_indentation);
+        }
+        if indent_innards {
+            print!("{}", buffer_indentation);
+        }
+        println!("{}", line);
+    }
+    print!("{}", line_indentation);
+    println!("{}", surrounding_right);
     Ok(())
 }
