80e2ad366a920a12f89d630fd03d569bd7677ef2
Axlefublr <axlefublr.ls@gmail.com>
2026-02-15 05:01:18 +0800

    change src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,47 @@
 pub mod prelude;
+
+pub trait CharExt {
+    fn is_indentation(&self) -> bool;
+}
+
+impl CharExt for char {
+    fn is_indentation(&self) -> bool {
+        *self == ' ' || *self == '\t'
+    }
+}
+
+pub fn alias_boundary_left(delimiter: &str) -> &str {
+    match delimiter {
+        "b" => "(",
+        "B" => "{",
+        "t" => "<",
+        "s" => "[",
+        "p" => "|",
+        "b→" => ")",
+        "B→" => "}",
+        "t→" => ">",
+        "s→" => "]",
+        "backtick" => "`",
+        "doublequote" => "\"",
+        "singlequote" => "'",
+        "triple-backtick" => "```",
+        "triple-doublequote" => r#"""""#,
+        "triple-singlequote" => "'''",
+        other => other,
+    }
+}
+
+pub fn boundary_left_to_right(delimiter: &str) -> &str {
+    match delimiter {
+        "(" => ")",
+        "{" => "}",
+        "<" => ">",
+        "[" => "]",
+        ")" => "(",
+        "}" => "{",
+        ">" => "<",
+        "]" => "[",
+        "begin" => "end",
+        other => other,
+    }
+}
