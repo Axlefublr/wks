5e275f7fba9539862a7a8f30ecdf048ed7382203
Axlefublr <axlefublr.ls@gmail.com>
2026-07-20 13:22:35 +0800

    change src/bin/anki-add-card.rs

--- src/bin/anki-add-card.rs
+++ src/bin/anki-add-card.rs
@@ -4,28 +4,21 @@
 
 use wks::prelude::*;
 
-const VALID_DECKS: [&str; 2] = ["Once", "Freq"];
 const VALID_NOTE_TYPES: [&str; 5] = ["b", "d", "f", "fb", "h"];
 
 fn main() -> Result<()> {
-    let contents = fs::read_to_string("/home/axlefublr/.cache/mine/anki-card.html").expect("read card file");
+    let contents = fs::read_to_string("/home/axlefublr/fes/zufi/anki-card.html").expect("read card file");
     let mut lines = contents.lines().map(str::trim);
 
-    let deck = lines.next().unwrap();
-    ensure!(!VALID_DECKS.contains(&deck), "invalid deck");
     let note_type = lines.next().unwrap();
-    ensure!(!VALID_NOTE_TYPES.contains(&note_type), "invalid note type");
+    ensure!(VALID_NOTE_TYPES.contains(&note_type), "invalid note type");
 
     let mut lines: Vec<_> = lines.collect();
     lines.insert(0, note_type);
-    lines.insert(0, deck);
+    lines.insert(0, "Once");
 
     let fields = lines.len();
-    ensure!(
-        !(4..=6).contains(&fields),
-        "expected 4..=6 fields, got {}",
-        fields
-    );
+    ensure!((4..=6).contains(&fields), "expected 4..=6 fields, got {}", fields);
 
     lines.resize(6, "");
 
